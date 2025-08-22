62fd706446c9911bfe30a1b1df0c2b54dce4c769
SAMEER MISHRA <sameermishra280202@gmail.com>
2025-08-22 14:59:30 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="SAMEER MISHRA",
+    author_email="sameermishra280202@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
